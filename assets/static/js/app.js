a4039eea5ed38dae3706e5f61f2c58743bb88965
ALLCAPs788 <capspfifer@gmail.com>
2021-01-10 20:20:05 -0500

    scatter template

---
+++ assets/static/js/app.js
@@ -0,0 +1,59 @@
+/*You need to create a scatter plot between two of the data variables such as Healthcare vs. Poverty or Smokers vs. Age.
+Using the D3 techniques we taught you in class, create a scatter plot that represents each state with circle elements. 
+You'll code this graphic in the app.js file of your homework directory—make sure you pull in the data from data.csv by using the d3.csv function. 
+Your scatter plot should ultimately appear like the image at the top of this section.
+
+
+Include state abbreviations in the circles.
+
+
+Create and situate your axes and labels to the left and bottom of the chart.
+
+
+Note: You'll need to use python -m http.server to run the visualization. This will host the page at localhost:8000 in your web browser.
+*/
+
+//scatter dimensions
+var margin = {top: 10, right: 30, bottom: 30, left: 60},
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var svg = d3.select("#my_dataviz")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+
+//import csv
+
+d3.csv("state_income_csv.csv");
+
+  // Add X axis
+  var x = d3.scaleLinear()
+    .domain([0, 4000])
+    .range([ 0, width ]);
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
+
+  // Add Y axis
+  var y = d3.scaleLinear()
+    .domain([0, 500000])
+    .range([ height, 0]);
+  svg.append("g")
+    .call(d3.axisLeft(y));
+
+  // Add dots
+  svg.append('g')
+    .selectAll("dot")
+    .data(data)
+    .enter()
+    .append("circle")
+      .attr("cx", function (d) { return x(d.GrLivArea); } )
+      .attr("cy", function (d) { return y(d.SalePrice); } )
+      .attr("r", 1.5)
+      .style("fill", "#69b3a2")
